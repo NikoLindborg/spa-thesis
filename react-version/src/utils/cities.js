ded8fb6a494d6d4897f25400e438f6e8edabb318
Niko Lindborg <niko.lindborg@Nikos-Laptop.local>
2023-09-23 12:40:34 +0300

    setup http client

---
+++ react-version/src/utils/cities.js
@@ -0,0 +1,7 @@
+const espoo = { id: 660129, city: 'Espoo' }
+const tampere = { id: 634963, city: 'Tampere' }
+const jyvaskyla = { id: 655195, city: 'Jyväskylä' }
+const kuopio = { id: 650225, city: 'Kuopio' }
+const cities = [espoo, tampere, jyvaskyla, kuopio]
+
+export { cities }
---
+++ vue-version/src/utils/http.js
@@ -0,0 +1,17 @@
+// simple http client
+const api_key = import.meta.env.VUE_APP_API_KEY
+const base_url = import.meta.env.BASE_URL
+
+const doFetch = async (url, options) => {
+  const response = await fetch(`${base_url}${url}&appid=${api_key}`, options)
+  const json = await response.json()
+  if (json.error) {
+    throw new Error(json.message + ': ' + json.error)
+  } else if (!response.ok) {
+    throw new Error('Something went wrong fetching')
+  } else {
+    return json
+  }
+}
+
+export { doFetch }
